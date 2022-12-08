45ca51a1571dd01db31c14a5f7ccb48f0a01c9ee
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2022-12-08 12:32:13 +0800

    fix(fs): solve uv conflict between threads
    - Moved wrongly called async copier from worker to uv.
    
    Possibly fixed #4

--- src/core/op/Flipper.cc
+++ src/core/op/Flipper.cc
@@ -15,36 +15,44 @@ namespace Op
 void
 flipInstall(Flow *flow, FlowCallback cb)
 {
+  cb(AL_FLIPDIR);
   Sys::runOnWorkerThread([=]() -> void {
-    cb(AL_FLIPDIR);
     auto base = getInstallPath();
     std::list<std::string> files = scanDirectory(base);
     int *total = new int(files.size());
     int *count = new int(0);
+
+    // Run sync ops first
     for(auto &f : files)
       {
         auto target = getStoragePath(std::filesystem::relative(f, base));
         mkParentDirs(target);
-        copyFileAsync(f, target, [=](bool stat) -> void {
-          (*count)++;
-          if(*count == *total)
-            {
-              delete count;
-              delete total;
-              Sys::runOnWorkerThread([=]() -> void {
-                try
-                  {
-                    std::filesystem::remove_all(base); // Delete source
-                    cb(AL_OK);
-                  }
-                catch(std::exception &e)
-                  {
-                    cb(AL_ERR);
-                  }
-              });
-            }
-        });
       }
+    Sys::runOnUVThread([=]() -> void {
+      for(auto &f : files)
+        {
+          auto target = getStoragePath(std::filesystem::relative(f, base));
+          copyFileAsync(f, target, [=](bool stat) -> void {
+            (*count)++;
+            if(*count == *total)
+              {
+                delete count;
+                delete total;
+                Sys::runOnWorkerThread([=]() -> void {
+                  try
+                    {
+                      std::filesystem::remove_all(base); // Delete source
+                      cb(AL_OK);
+                    }
+                  catch(std::exception &e)
+                    {
+                      cb(AL_ERR);
+                    }
+                });
+              }
+          });
+        }
+    });
   });
 }
 
