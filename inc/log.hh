caa334bc566c7234ef171ea4390754b054ab77e8
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2022-12-13 22:22:41 +0800

    perf(log): allow log system to be quiet  in prod
    - Added macro to suppress log outputs.

--- inc/log.hh
+++ inc/log.hh
@@ -1,8 +1,12 @@
 #ifndef INCLUDE_LOG
 #define INCLUDE_LOG
 
+#ifndef ACH_LOG_QUIET
 #include <iostream>
 #define LOG(text)                                                             \
   std::cout << "[" __FILE__ << ":" << __LINE__ << "] " << text << "\n";
+#else
+#define LOG(test)
+#endif
 
 #endif /* INCLUDE_LOG */
