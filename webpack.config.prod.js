95e613bf9e711abb800edf7637af874b1e0a51e7
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2023-01-11 21:01:56 +0800

    chore(build): split dev and prod build
    - Seperated dev and prod build config for webpack.

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    Main: "./src/uic/App.tsx",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    pathinfo: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            experimentalWatchApi: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ya?ml$/,
+        use: "yaml-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx", ".css", ".yaml"],
+  },
+  mode: "production",
+};
