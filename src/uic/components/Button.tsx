16c2ba259679b9f7d2d41c18e8687d3cd10aff64
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2023-02-04 11:34:30 +0800

    fix(ui): fix wrong frontend data
    - Removed press-to-confirm button.
    
    Fixed #41
    CHECKED

--- src/uic/components/Button.tsx
+++ src/uic/components/Button.tsx
@@ -33,30 +33,6 @@ export function Button(props: ButtonProps): JSX.Element {
   if (props.multiSelect && props.selected) {
     cname += " hv";
   }
-  const confirmProg = useRef(0);
-  const [confirmProgDisplay, setProg] = useState(0);
-  const mouseDown = useRef(false);
-  const interval = useRef(-1);
-
-  const progress = () => {
-    if (props.multiSelect) {
-      return;
-    }
-    if (mouseDown.current) {
-      confirmProg.current += 2;
-      setProg(confirmProg.current);
-      if (confirmProg.current >= 100) {
-        confirmProg.current = 0;
-        if (props.onClick) {
-          mouseDown.current = false;
-          props.onClick();
-        }
-      }
-    } else {
-      confirmProg.current = 0;
-      setProg(confirmProg.current);
-    }
-  };
 
   return (
     <>
@@ -67,21 +43,10 @@ export function Button(props: ButtonProps): JSX.Element {
             props.hint == "warn" ? "var(--a2-warn)" : "var(--a2-base)",
           overflow: "hidden",
         }}
-        onMouseDown={() => {
-          mouseDown.current = true;
-          interval.current = setInterval(progress, 15) as unknown as number;
-        }}
         onClick={() => {
-          if (props.multiSelect && props.onClick) {
+          if (props.onClick) {
             props.onClick();
           }
-          clearInterval(interval.current);
-          setProg(0);
-        }}
-        onMouseUp={() => {
-          mouseDown.current = false;
-          clearInterval(interval.current);
-          setProg(0);
         }}
         className={cname}
         onMouseEnter={() => {
@@ -112,17 +77,6 @@ export function Button(props: ButtonProps): JSX.Element {
             {props.sub || ""}
           </span>
         </div>
-        <div
-          style={{
-            position: "absolute",
-            backgroundColor: "#89898950",
-            width: confirmProgDisplay + 5 + "%",
-            height: "100%",
-            bottom: 0,
-            left: "-5%",
-            overflow: "hidden",
-          }}
-        ></div>
       </div>
     </>
   );
